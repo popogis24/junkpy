ccc683f279600ee61bba09017b7ea71dd48ea033
anderson stolfi <133429641+popogis24@users.noreply.github.com>
2023-12-22 16:50:17 -0300

    Update mapbio_quantitativo.py

--- mapbio_quantitativo.py
+++ mapbio_quantitativo.py
@@ -5,8 +5,8 @@ from arcpy.sa import *
 
 arcpy.env.addOutputsToMap = False
 arcpy.env.overwriteOutput = True
-dataset = r"R:\\09-Banco_De_Dados_Geografico\\01-Clientes\\Neoenergia\\Lote1_24\\GDB_22_12_23\\Dados_Lote14.gdb\\Tracados"
-fuso = '24S'
+dataset = r"R:\\09-Banco_De_Dados_Geografico\\01-Clientes\\Neoenergia\\Lote1_24\\GDB_22_12_23\\Dados_Lote6.gdb\\Tracados"
+fuso = '23S'
 mapbiomas = fr'C:\Users\anderson.souza\Downloads\rtp_mapbiomas_{fuso}.shp'
 buff = 40
 arcpy.env.workspace = dataset
@@ -18,9 +18,8 @@ for fc in fclist:
         filename = 'Uso_Solo_'+fc  
         output = arcpy.analysis.PairwiseBuffer(fc, os.path.join(caminho, filename), buff)
         caminho_clip = fr'R:\09-Banco_De_Dados_Geografico\01-Clientes\Neoenergia\Lote1_24\Outros\Quantitativo_Uso_Solo\{fuso}\Clip'
-
-        clipped = arcpy.analysis.PairwiseClip(mapbiomas, output, os.path.join(caminho_clip, filename))
-        dissolved = arcpy.analysis.PairwiseDissolve(clipped, os.path.join(caminho_clip,filename+'_diss'),'Classes')
+        clipped = arcpy.analysis.PairwiseIntersect([mapbiomas, output], os.path.join(caminho_clip, filename))
+        dissolved = arcpy.analysis.PairwiseDissolve(clipped, os.path.join(caminho_clip,filename+'_diss'),['Classes','Name'])
         arcpy.management.CalculateField(dissolved,'Area_ha','!shape.area@hectares!')
         caminho_excel = r'R:\09-Banco_De_Dados_Geografico\01-Clientes\Neoenergia\Lote1_24\Outros\Quantitativo_Uso_Solo\Planilhas'
         arcpy.conversion.TableToExcel(os.path.join(caminho_clip,filename+'_diss'),os.path.join(caminho_excel,filename+'.xlsx'))
